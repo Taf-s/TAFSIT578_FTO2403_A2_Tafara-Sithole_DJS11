4a2f1f89353363828a421e93452eb93163e125bb
taf-s <tafara.sithole@gmail.com>
2024-08-15 16:50:38 +0200

    updated styling to the showcards on the homepage component

--- src/components/ShowCard.js
+++ src/components/ShowCard.js
@@ -3,29 +3,31 @@ import { Link } from "react-router-dom";
 
 function ShowCard({ show }) {
   return (
-    <div className="bg-white shadow-lg rounded-lg overflow-hidden">
+    <div className="bg-white shadow-lg rounded-lg overflow-hidden relative transition duration-300 transform hover:scale-105 hover:translate-y-2 hover:rotate-0 hover:shadow-2xl">
       <Link to={`/show/${show.id}`}>
         <img
           src={show.image}
           alt={show.name}
-          className="w-full h-48 object-cover"
+          className="w-full h-full object-cover"
         />
       </Link>
-      <div className="p-4">
-        <h2 className="text-2xl font-bold">{show.title}</h2>
-        <p className="text-gray-600"> {show.seasons} Seasons</p>
-        <p className="text-gray-600">
-          Last Updated: {new Date(show.updated).toLocaleDateString()}
-        </p>
-        <div className="mt-2">
-          {show.genres.map((genre) => (
-            <span
-              genre={genre}
-              className="inline-block bg-gray-200 text-gray-800 text-sm px-2 py-1 rounded-full mr-2"
-            >
-              {genre}
-            </span>
-          ))}
+      <div className="absolute bottom-3 left-3 right-3 p-4 bg-black bg-opacity-50 rounded-md flex flex-col justify-center items-left h-1/3">
+        <div className="text-white">
+          <h2 className="text-lg font-bold">{show.title}</h2>
+          <p className="text-sm text-gray-200"> {show.seasons} Seasons</p>
+          <p className="text-sm text-gray-200">
+            Last Updated: {new Date(show.updated).toLocaleDateString()}
+          </p>
+          <div className="mt-2 flex flex-wrap">
+            {show.genres.map((genre) => (
+              <span
+                genre={genre}
+                className="inline-block bg-gray-200 text-gray-800 text-xs px-1 py-0.5 rounded-full mr-1 mb-1"
+              >
+                {genre}
+              </span>
+            ))}
+          </div>
         </div>
       </div>
     </div>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -12,7 +12,6 @@ function HomePage() {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-4xl font-bold mb-8">All Shows</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {filteredShows.map((show) => (
           <ShowCard key={show.id} show={show} />
