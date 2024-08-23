16c93ce22ca030a44ce43cce669625e4699fc5b0
taf-s <tafara.sithole@gmail.com>
2024-08-24 01:55:30 +0200

    added carousel to homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,6 @@
 import React, { useContext, useState, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 import { SearchContext } from "../SearchProvider";
 import ShowCard from "../components/ShowCard";
 import sortShows from "../utils/sortShows";
@@ -24,24 +26,59 @@ function HomePage() {
     setSortedShows(sorted);
   }, [showsToSort, sortBy]);
 
+  const carouselShows = sortedShows.slice(0, 5);
+
   return (
-    <div className="container mx-auto px-4 py-8">
-      <div className="flex justify-end mb-4 mt-20">
-        <select
-          value={sortBy}
-          onChange={(e) => setSortBy(e.target.value)}
-          className="bg-customBackground hover:bg-customRed rounded-md p-2 mb-4"
+    <div>
+      {/* Carousel Section */}
+      <div className="my-8 ">
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          centeredSlides={true}
+          slidesPerView={2}
+          spaceBetween={10}
+          slidesPerGroup={1}
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
         >
-          <option value="title-asc">A-Z</option>
-          <option value="title-desc">Z-A</option>
-          <option value="date-asc">Oldest</option>
-          <option value="date-desc">Newest</option>
-        </select>
+          {carouselShows.map((show) => (
+            <SwiperSlide key={show.id} className="w-full">
+              <div className="p-2">
+                <ShowCard show={show} className="h-40" />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {sortedShows.map((show) => (
-          <ShowCard key={show.id} show={show} />
-        ))}
+      <div className="container mx-auto px-4 py-8">
+        {/* Sort Dropdown */}
+        <div className="flex justify-end mb-4 mt-20">
+          <select
+            value={sortBy}
+            onChange={(e) => setSortBy(e.target.value)}
+            className="bg-customBackground hover:bg-customRed rounded-md p-2 mb-4"
+          >
+            <option value="title-asc">A-Z</option>
+            <option value="title-desc">Z-A</option>
+            <option value="date-asc">Oldest</option>
+            <option value="date-desc">Newest</option>
+          </select>
+        </div>
+
+        {/* Grid of Shows */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {sortedShows.map((show) => (
+            <ShowCard key={show.id} show={show} />
+          ))}
+        </div>
       </div>
     </div>
   );
