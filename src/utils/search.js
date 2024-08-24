2be7c606f683613f535aa3c45ea6f5b323d723e5
taf-s <tafara.sithole@gmail.com>
2024-08-24 14:19:04 +0200

    implemented genre mappijng to the search shows function

--- src/utils/search.js
+++ src/utils/search.js
@@ -1,7 +1,11 @@
+import { genreMapping } from "./genreUtils";
+
 export function searchShows(shows, query) {
   return shows.filter((show) => {
     const showTitle = show.title?.toLowerCase() || "";
-    const showGenres = show.genres || [];
+    const showGenres = show.genres.map((genreId) =>
+      genreMapping[genreId].toLowerCase()
+    );
 
     return (
       showTitle.includes((query || "").toLowerCase()) ||
